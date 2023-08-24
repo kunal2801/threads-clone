405d93d2942e1e929f6619e3e93448c15bdeaa36
Kunal <kunalsharma.280104@gmail.com>
2023-08-24 17:40:10 +0530

    Added functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,32 @@
-//app/page.tsx
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import { currentUser } from "@clerk/nextjs";
+import Pagination from "@/components/shared/Pagination";
 
-export default async function Home() {
-  const result = await fetchPosts(1, 30);
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No threads found</p>
@@ -20,7 +36,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id || ""}
+                currentUserId={user.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
@@ -32,6 +48,14 @@ export default async function Home() {
           </>
         )}
       </section>
+
+      <Pagination
+        path="/"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </>
   );
 }
+
+export default Home;
